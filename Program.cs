aaf63e6f3116aed304cef7b9160150a9b7a2e31a
mark <markram0100@gmail.com>
2022-10-20 11:20:16 -0700

    done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿//  mark ramirez
 //  started 10/19/2022
 //  mad lib story
+// it will ask you to select words and then use those word to fill out a story and hopefully it makes sense
 
 Console.Clear();
 

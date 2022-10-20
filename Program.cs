7850c160b3b80fbfc959df9566ce7a053eef0b07
mark <markram0100@gmail.com>
2022-10-20 11:20:31 -0700

    done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿//  mark ramirez
-//  started 10/19/2022
+//  started 10/20/2022
 //  mad lib story
 // it will ask you to select words and then use those word to fill out a story and hopefully it makes sense
 
